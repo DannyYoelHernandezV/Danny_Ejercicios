65787cb4bb8a24c4b406ded5bd16181e1a11d810
DannyYoelHernandezV <140682467+DannyYoelHernandezV@users.noreply.github.com>
2024-11-06 14:22:28 -0600

    Add files via upload

---
+++ colaespecial.py
@@ -0,0 +1,21 @@
+class MYLIST(object):
+  mi_lista=[]
+  mi_lista.append("Maria")
+  mi_lista.append("Juan")
+  mi_lista.append("Carlos")
+  mi_lista.append("Danny")
+  mi_lista.append("Sofia")
+  mi_lista.append("Axel")
+  mi_lista.append("Javier")
+  mi_lista.append("Bryan")
+  mi_lista.append("Rober")
+  mi_lista.append("Aby")
+  
+  
+  
+  
+  
+  
+  
+  
+  
